a0e85261efdfb42e8d86da7f2145ace1e6eee9dd
Hasib072 <hasibrahaman72@gmail.com>
2023-11-22 22:45:38 +0530

    Connected MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,22 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
 app.use(cors());
+
+const DB = process.env.MONGO_URI;
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB Connection Succesfull !!!!");
+}).catch((err) => console.log('DB Connection Error :' + err));
+
+
 
 app.get('/', (req,res) => {
     res.send("api is runnnig!!");
