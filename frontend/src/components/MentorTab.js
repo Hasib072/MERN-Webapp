3701ad5b1c306f17e205e9ea78a62c0f77c42e1f
hasib72 <hasibrahaman72@gmail.com>
2023-12-19 00:04:38 +0530

    mentor new Session

--- frontend/src/components/MentorTab.js
+++ frontend/src/components/MentorTab.js
@@ -1,20 +1,32 @@
+import React, { useState } from 'react';
+
+
 function MentorTab(){
+    const [isExpanded, setIsExpanded] = useState(false);
+    
+    const toggleCard = () => {
+        setIsExpanded(!isExpanded);
+      };
 
     return(
         <div className="col main">
             {/* <!-- Main content goes here --> */}
             {/* <h1>Mentor Tab Body 2 !</h1> */}
             <div className="mentoring_sec1 container">
-                <input placeholder="Enter Register No." class="input-style row" type="text"/>
+                <input placeholder="Enter Register No." class="input reg_input row" type="text"/>
                 <div className="row">
                     <p className="ms1-txt-style"><span>Mentee Name :&emsp; </span> FirstName LastName &emsp;&emsp; |  &ensp;&ensp; 2241XXX &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <span>Class : </span> X_XXX</p>
                 </div>
             </div>
-            <div className="mentoring_sec2 container">
-                <div className="row">
-                    <p className="sectionheadings">Last Session Details</p>
-                    
+            <div className={`container ${isExpanded ? 'mentoring_sec2_exp' : 'mentoring_sec2'}`}>
+                <div className="row" onClick={toggleCard}>
+                    <p className="sectionheadings pointerarrow" >Last Session Details</p>
+                    <img className="steppers pointerarrow" src="assets/steppers_.svg" alt="expand" />
                 </div>
+                    {isExpanded && ( <div>
+
+                    
+                    
                 <div className="row">
                     <p className="ms2-txt-style"><span>Date :&emsp; </span> DD/MM/YYYY &emsp;&emsp;  &ensp;&ensp; &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <span>Time : &ensp;&ensp;</span> hh:mm AM</p>
                 </div>
@@ -33,7 +45,35 @@ function MentorTab(){
                 <div className="row">
                     <p className="sectionsubheadings">Remarks</p>
                 </div>
+
+                </div>)}
             </div>
+
+            <div className='container mentoring_sec2_exp'>
+                <div className="row">
+                    <p className="sectionheadings" >New Session Details</p>
+                    {/* <img className="editicon" src="assets/steppers_.svg" alt="expand" /> */}
+                </div>
+                <div className="row DndT">
+                    <p className="ms2-txt-style"><span>Date :&emsp; </span> DD/MM/YYYY &emsp;&emsp;  &ensp;&ensp; &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <span>Time : &ensp;&ensp;</span> hh:mm AM</p>
+                </div>
+                <div className="row">
+                    <p className="sectionheadings2">Main Topic of discussion</p>
+                    <input class="input topic_input" name="text" type="text" placeholder="Title..."></input>
+                </div>
+                <div className="row">
+                    <p className="sectionheadings2">Keywords :</p>
+                </div>
+                <div className='tag-container'>
+                    <div className='tag'>
+                        Sample Tag
+                    </div>
+                </div>
+                <div>
+                    <input class="input tag_input" name="text" type="text" placeholder="Add Keywords..."></input>
+                </div>
+            </div>
+            
         </div>
     );
 
