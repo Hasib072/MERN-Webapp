093942058b6ddf7cbc547529fb236af186a6334f
hasib72 <hasibrahaman72@gmail.com>
2023-12-27 23:49:34 +0530

    Admin Rows

--- frontend/src/components/AdminTab.js
+++ frontend/src/components/AdminTab.js
@@ -3,7 +3,28 @@ function ReportsTab(){
     return(
         <div className="col main">
             {/* <!-- Main content goes here --> */}
-            <h1>. . . Admin Tab Body coming soon !</h1>
+            <div className="at_box row">
+                <div className="mentor_section col">
+                    <div className="mentor_row">
+                        <div className="mentor_row_detail1">
+                        <p>Mentor Name | Mentor ID</p>
+                        </div>
+                        <div className="mentor_row_detail2">
+                        <p>Mentor no : xx</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mentee_section col">
+                <div className="mentor_row">
+                        <div className="mentor_row_detail1">
+                        <p>Mentee Name | RegNo: 2241XXX</p>
+                        </div>
+                        <div className="mentor_row_detail2">
+                        <p>No of Session : xx</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 
--- frontend/src/components/Reports.js
+++ frontend/src/components/Reports.js
@@ -3,7 +3,14 @@ function ReportsTab(){
     return(
         <div className="col main">
             {/* <!-- Main content goes here --> */}
-            <h1>. . . Reports Tab Body coming soon !</h1>
+            <div className="mentoring_sec1 container">
+                <form>
+                    <input placeholder="Enter Register No." class="input reg_input row" type="text"/>
+                </form>
+                <div className="row">
+                    <p className="ms1-txt-style"><span>Mentee Name :&emsp; </span> FirstName LastName &emsp;&emsp; |  &ensp;&ensp; 2241XXX &ensp;&ensp;&ensp;&ensp;&ensp;&ensp; <span>Class : </span> X_XXX</p>
+                </div>
+            </div>
         </div>
     );
 
